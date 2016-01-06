6dd1d546bc7ac31399b6b5f5ced1f899174367a0
Andrew <andy_htun@hotmail.com>
2016-01-06 14:48:04 +0000

    reset function added

--- jasmine-standalone-2.4.1_3/spec/ThermostatSpec.js
+++ jasmine-standalone-2.4.1_3/spec/ThermostatSpec.js
@@ -15,6 +15,10 @@ describe("Thermostat", function(){
     thermostat.up();
     expect(thermostat.temperature).toEqual(21);
   });
+  it('raises an error when going above maximum temperature', function(){
+    thermostat.temperature = 25;
+    expect(function(){thermostat.up();}).toThrowError("Cannot go above maximum temperature");
+  });
 
   it('decrease temperature by 1 with down button', function(){
     thermostat.down();
@@ -29,9 +33,23 @@ describe("Thermostat", function(){
     thermostat.temperature = 10;
     expect(function(){thermostat.down();}).toThrowError("Minimum temperature is 10 degrees")
   });
+  it('throws an error when power saving mode is already on', function(){
+    expect(function(){thermostat.turnOnPowerSaving();}).toThrowError("Power saving mode is already on");
+  });
 
   it('has a maximum temperature 25 when power saving mode is switched on', function(){
+    thermostat.turnOffPowerSaving();
     thermostat.turnOnPowerSaving();
     expect(thermostat.maximum_temperature).toEqual(25);
+  });
+
+  it('changes maximum temperature back to 25 when power saving mode is turned off', function(){
+    thermostat.turnOffPowerSaving();
+    expect(thermostat.maximum_temperature).toEqual(32);
+  });
+  it('resets temperature back to 20 with reset', function(){
+     thermostat.up();
+     thermostat.reset();
+     expect(thermostat.temperature).toEqual(20);
   });
 });
--- jasmine-standalone-2.4.1_3/src/Thermostat.js
+++ jasmine-standalone-2.4.1_3/src/Thermostat.js
@@ -3,18 +3,28 @@
 function Thermostat(){
   this.temperature = 20
   this.minimum_temperature = 10
-  this.maximum_temperature = 32
+  this.maximum_temperature = 25
 }
 
 Thermostat.prototype.up = function(){
+  if(this.temperature >= this.maximum_temperature){
+    throw new Error("Cannot go above maximum temperature");
+  };
   return this.temperature += 1;
 };
 Thermostat.prototype.down = function(){
   if(this.temperature <= this.minimum_temperature){
-    throw new Error("Minimum temperature is 10 degrees")
+    throw new Error("Minimum temperature is 10 degrees");
   };
   return this.temperature -= 1;
 };
 Thermostat.prototype.turnOnPowerSaving = function(){
-  return this.maximum_temperature = 25;
+  if(this.maximum_temperature === 25) throw new Error("Power saving mode is already on");
+  else (this.maximum_temperature = 25);
+};
+Thermostat.prototype.turnOffPowerSaving = function(){
+  return this.maximum_temperature = 32;
+};
+Thermostat.prototype.reset = function(){
+  return this.temperature = 20;
 };
