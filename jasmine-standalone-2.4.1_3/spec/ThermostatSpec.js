4dce66ee04dd59ef183551e8e630b1f450941e54
Andrew <andy_htun@hotmail.com>
2016-01-06 14:18:56 +0000

    power saving mode added

--- jasmine-standalone-2.4.1_3/spec/ThermostatSpec.js
+++ jasmine-standalone-2.4.1_3/spec/ThermostatSpec.js
@@ -21,8 +21,17 @@ describe("Thermostat", function(){
     expect(thermostat.temperature).toEqual(19);
   });
 
-  it('has a minimum temperature of 10', function(){
+  it('shows minimum temperature', function(){
+    expect(thermostat.minimum_temperature).toEqual(10);
+  });
+
+  it('throws an error when going below minimum temperature', function(){
     thermostat.temperature = 10;
     expect(function(){thermostat.down();}).toThrowError("Minimum temperature is 10 degrees")
+  });
+
+  it('has a maximum temperature 25 when power saving mode is switched on', function(){
+    thermostat.turnOnPowerSaving();
+    expect(thermostat.maximum_temperature).toEqual(25);
   });
 });
--- jasmine-standalone-2.4.1_3/src/Thermostat.js
+++ jasmine-standalone-2.4.1_3/src/Thermostat.js
@@ -3,6 +3,7 @@
 function Thermostat(){
   this.temperature = 20
   this.minimum_temperature = 10
+  this.maximum_temperature = 32
 }
 
 Thermostat.prototype.up = function(){
@@ -13,4 +14,7 @@ Thermostat.prototype.down = function(){
     throw new Error("Minimum temperature is 10 degrees")
   };
   return this.temperature -= 1;
+};
+Thermostat.prototype.turnOnPowerSaving = function(){
+  return this.maximum_temperature = 25;
 };
