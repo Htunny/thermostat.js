0420e64a1b7888ab0fd282cca474cecafb01beb4
Andrew <andy_htun@hotmail.com>
2016-01-07 11:33:24 +0000

    initial ajax commit

--- jasmine-standalone-2.4.1_3/src/interface.js
+++ jasmine-standalone-2.4.1_3/src/interface.js
@@ -1,3 +1,16 @@
+$.ajax({
+
+url:"api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4db7936adac8e360e07f2f6e0b70d42f",
+
+type: "GET",
+
+dataType : "json",
+
+success: function( json ) {
+        $( "#weather" ).text( json.title ).appendTo( "body" );
+        id
+});
+
 $(document).ready(function() {
   var thermostat = new Thermostat();
 
@@ -5,7 +18,7 @@ $(document).ready(function() {
     $('#current-temperature').text(thermostat.temperature);
     $('#energy-usage').text(thermostat.energyUsage());
   };
-  
+
   energytemp();
 
   $('.temperature-change').click(function() {
@@ -27,5 +40,7 @@ $(document).ready(function() {
     thermostat.turnOffPowerSaving();
     $('#power-saving-status').text('off')
   })
+
+
 
 });
